ac39852c22405a8c2be51bde895ebf0031cf7c78
benjidaluz <111111029+benjidaluz@users.noreply.github.com>
2022-09-04 02:20:12 -0700

    Time: 52 ms (20.46%), Space: 13.6 MB (35.66%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -1,15 +1,12 @@
 class Solution(object):
     def runningSum(self, nums):
         i = 0
-        x = 0
         arr=[0]*len(nums)
         while i < len(nums):
             if i == 0:
-                x = nums[i]
                 arr[i] = nums[i]
                 i = i + 1
-            x = x + nums[i]
-            arr[i] = x
+            arr[i] = arr[i-1] + nums[i]
             i = i + 1
         else:
             return arr
